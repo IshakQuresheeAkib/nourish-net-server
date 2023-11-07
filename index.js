502aaf58dd357a35434440b19436ae7c769279db
IshaqQuresheeAkib <akibqureshee@gmail.com>
2023-11-07 10:05:43 +0600

    created api for deleting data from food request collection

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config(); 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 const port = process.env.PORT || 5000;
 const app = express()
  
-app.use(cookieParser())
+
 app.use(cors({
   origin:['http://localhost:5173','http://localhost:5174'],
   credentials:true
 }))
 app.use(express.json())
-
+app.use(cookieParser())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vuba6ki.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -25,6 +25,28 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
+
+// middlewares
+const verify = async (req,res,next)=>{
+  const token = req?.cookies?.token
+  // console.log(token)
+  if (!token) {
+      return res.status(401).send({error:'Forbidden access',status:401})
+  }
+  jwt.verify(token,process.env.SECRET_KEY,(err,decode)=>{
+      if (err) {
+          console.log(err);
+          return res.status(403).send({error:'wrong access',status:401})
+      }
+      req.decode = decode;
+      next();
+  })
+  
+}
+
+
+
 async function run() {
   try {
     const foodsCollection = client.db('nourishNetDB').collection('foodsCollection')
@@ -34,9 +56,36 @@ async function run() {
         res.send('Hello World!')
       })
 
+      app.post('/jwt',async(req,res)=>{
+        const body = req.body;
+        const token = jwt.sign(body,process.env.SECRET_KEY,{expiresIn:'1h'})
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production', 
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            
+        })               
+        .send({message:'SUCCESS',token})
+    })
+
+    app.post('/logout',async(req,res)=>{
+      const user = req.body;
+      console.log(user);
+
+          res.clearCookie(
+          "token",
+          {
+          maxAge: 0,
+          secure: process.env.NODE_ENV === "production" ? true: false,
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          }
+          )
+          .send({message:'Logged out'})
+  })
+
     // api for available foods section
     app.get('/available-foods',async(req,res)=>{
-      const cursor = foodsCollection.find();
+      const cursor = foodsCollection.find({ foodQuantity: { $gt: 0 } });
       const foods = await cursor.toArray();
       res.send(foods)
     })
@@ -51,7 +100,6 @@ async function run() {
       const food = req.body;
       const { foodName,foodImage,foodQuantity,donatorEmail,donatorImage,donatorName,pickupLocation,expiredDateTime,additionalNotes,foodStatus } = food
       const filter = {_id: new ObjectId(food._id)}
-      console.log(filter);
       const options = {upsert:true}
       const updatedFood = {
           $set:{
@@ -61,6 +109,26 @@ async function run() {
       const result = await foodsCollection.updateOne(filter,updatedFood,options)
       res.send(result)
   })
+    app.patch('/available-foods',async(req,res)=>{
+      const {id,foodQuantity} = req.body;
+      const newQuantity = foodQuantity - 1
+      // if(!newQuantity){
+      //   const query = {_id: new ObjectId(id)}
+      //   const result = await foodsCollection.deleteOne(query);
+      //   return res.send(result);        
+      // }
+      const filter = {_id: new ObjectId(id)}
+      const options = {upsert:true}
+      const updatedFood = {
+          $set:{
+            foodQuantity: newQuantity
+          }
+      }
+      const result = await foodsCollection.updateOne(filter,updatedFood,options)
+      res.send(result)
+  })
+
+
 
     app.get('/available-foods/:id',async(req,res)=>{
       const id = req.params.id
@@ -69,9 +137,13 @@ async function run() {
       res.send(food)
     })
 
-    app.delete('/available-foods/:id',async(req,res)=>{
-      const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+    app.delete('/available-foods/:name',async(req,res)=>{
+      const name = req.params?.name;
+      console.log(name);
+      let query;
+      if (name) {
+        query = {foodName: name}
+      }  
       const result = await foodsCollection.deleteOne(query);
       res.send(result)
     })
@@ -92,16 +164,50 @@ async function run() {
     app.post('/requested-food',async(req,res)=>{
       const food = req.body;
       const result = await foodRequestCollection.insertOne(food)
+      res.send(result);
+    })
+
+    app.get('/requested-food',async(req,res)=>{
+      const name = req.query?.name;
+      let query;
+      if (name) {
+        query = {foodName: name}
+      }
+      const result = await foodRequestCollection.find(query).toArray();
+      res.send(result)
+    })
+    app.get('/requested-food',async(req,res)=>{
+      const email = req.query?.email;
+      let query;
+      if (email) {
+        query = {email: email}
+      }
+      const result = await foodRequestCollection.find(query).toArray();
       res.send(result)
     })
 
-    app.get('/requested-food/:id',async(req,res)=>{
-      const id = req.params.id;
-      console.log(id);
-      const query = {_id: new ObjectId(id)}
-      const result = await foodRequestCollection.findOne(query)
+    app.patch('/requested-food',async(req,res)=>{
+      const {id,foodStatus} = req.body;
+      console.log(id,foodStatus);
+      const filter = {_id: new ObjectId(id)}
+      const options = {upsert:true}
+      const updatedFood = {
+          $set:{foodStatus}
+      }
+      const result = await foodRequestCollection.updateOne(filter,updatedFood,options)
       res.send(result)
     })
+
+    app.delete('/requested-food/:id',async(req,res)=>{
+      const id = req.params?.id;
+      let query;
+      if (id) {
+        query = {_id: new ObjectId(id)}
+      }
+      const result = await foodRequestCollection.deleteOne(query)
+      res.send(result)
+    })
+
 
     
     client.db("admin").command({ ping: 1 });
